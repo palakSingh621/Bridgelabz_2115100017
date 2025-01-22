369fbf59011a0e47cd6a677205787b14dfaa9bf5
palakSingh621 <kushwahps621@gmail.com>
2025-01-22 14:38:24 +0530

    final Assignment-2 solutions

--- Assignment-3/IntOperation.cs
+++ Assignment-3/IntOperation.cs
@@ -1,23 +1,24 @@
 using System;
-class DoubleOperation
+class IntOperation
 {
 	public static void Main(String[] args)
 	{
 		//taking value of 'a' form user
 		Console.Write("Enter first value: ");
-		double a=Convert.ToDouble(Console.ReadLine());
+		int a=Convert.ToInt32(Console.ReadLine());
 		//taking value of 'b' form user
 		Console.Write("Enter second value: ");
-		double b=Convert.ToDouble(Console.ReadLine());
+		int b=Convert.ToInt32(Console.ReadLine());
 		//taking value of 'c' form user
 		Console.Write("Enter third value: ");
-		double c=Convert.ToDouble(Console.ReadLine());
+		int c=Convert.ToInt32(Console.ReadLine());
 		
-		double v1=a+b*c;
-		double v2=a*b+c;
-		double v3= c+a/b;
-		double v4 = a%b+c;
+		int v1=a+b*c;
+		int v2=a*b+c;
+		int v3= c+a/b;
+		int v4 = a%b+c;
 		
-		Console.WriteLine("The result of Double Operations are "+v1.ToString("F2")+", "+v2.ToString("F2")+", "+v3.ToString("F2")+" and "+v4.ToString("F2"));
+		Console.WriteLine("The result of Int Operations are "+v1+", "+v2+", "+v3+" and "+v4);
 	}
 }
+
