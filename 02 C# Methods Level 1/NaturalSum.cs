2c342419c5e0101de1bf09bcbd9b22c7c2299797
palakSingh621 <kushwahps621@gmail.com>
2025-01-27 22:43:51 +0530

    Correcting Assignment names and Assignment-9 Solutions

---
+++ 02 C# Methods Level 1/NaturalSum.cs
@@ -0,0 +1,23 @@
+using System;
+class NaturalSum
+{
+	public static void Main(String[] args)
+	{
+		// Taking number as input from user
+		Console.Write("Enter a number: ");
+		int num = Convert.ToInt32(Console.ReadLine());
+		sum(num);
+	}
+	public static void sum(int num)
+	{
+		// Checking for Valid natural number
+		if( num >=0)
+		{
+			int sum=num * (num+1)/2;
+			Console.WriteLine("The sum of "+num+" natural numbers is "+sum);
+		}else{
+			Console.WriteLine("The number "+num+" is not a natural number");
+		}
+	}
+}
+		
---
+++ 02 C# Methods Level 1/Spring.cs
@@ -0,0 +1,22 @@
+using System;
+class Spring
+{
+	public static void Main(String[] args)
+	{
+		Console.Write("Enter the month number: ");
+		int month= Convert.ToInt32(Console.ReadLine());
+		Console.Write("Enter the day number: ");
+		int day= Convert.ToInt32(Console.ReadLine());
+		season(month, day);
+	}
+	public static void season(int month, int day)
+	{
+		// Checking for the Spring season
+		if((month==3 && day >=20 && day<=31)||(month==4 && day >=1 && day<=30)||(month==5 && day >=1 && day<=31)||(month==6 && day >=1 && day<=20)){
+			Console.WriteLine("It's a Spring Season.");
+		}else{
+			Console.WriteLine("Not a Spring Season.");
+		}
+	}
+}
+		
