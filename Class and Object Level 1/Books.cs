fb249c1a48fa500de4197ffc796f96d93580efb1
Palak Singh <107800373+palakSingh621@users.noreply.github.com>
2025-02-02 14:01:01 +0530

    Assignment-8 Solutions

---
+++ Class and Object Level 1/Books.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Object_and_Classes
+{
+    internal class Books
+    {
+        private string title;
+        private string author;
+        private double price;
+        //Constructor
+        public Books(string title, string author, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        //Method to display the details 
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Title : "+title);
+            Console.WriteLine("Author : " + author);
+            Console.WriteLine("Price :" + price);
+        }
+
+        //Main method 
+        static void Main(String[] args)
+        {
+            Books book1 = new Books("Becoming", "Michell Obama", 3000.00);
+            Console.WriteLine("Details of Book 1:");
+            book1.DisplayDetails();
+            Console.WriteLine();
+            Books book2 = new Books("The Palace of illusions", "Chitra Banerjee Divakaruni", 1000.00);
+            Console.WriteLine("Details of Book 2:");
+            book2.DisplayDetails();
+
+        }
+    }
+}
---
+++ Class and Object Level 1/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Object_and_Classes
+{
+    internal class Employee
+    {
+        // Fields (Attributes)
+        private string name;
+        private int id;
+        private double salary;
+
+        // Constructor
+        public Employee(string name, int id, double salary)
+        {
+            this.name = name;   
+            this.id = id;   
+            this.salary = salary;
+        }
+        // Method to display employee details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee Name:" + name);
+            Console.WriteLine("Employee ID: " + id);
+            Console.WriteLine("Salary: " + salary);
+        }
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Rajesh", 234, 30000.00);
+            Employee emp2 = new Employee("Palak", 167, 75000.00);
+            Console.WriteLine("Employee Detais: ");
+            Console.WriteLine();
+            Console.WriteLine("Employee 1 Details:");
+            emp1.DisplayDetails();
+            Console.WriteLine();
+            Console.WriteLine("Employee 2 Details: ");
+            emp2.DisplayDetails();
+        }
+    }
+}
