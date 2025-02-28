1749fce6ac07a018a34d2a10091e638f78e7dcef
Palak Singh <107800373+palakSingh621@users.noreply.github.com>
2025-02-28 23:03:09 +0530

    [Palak] Refactor . Adding Service folder to RL
    Signed-off-by: Palak Singh <107800373+palakSingh621@users.noreply.github.com>

---
+++ RepositoryLayer/Service/RegisterUserRL.cs
@@ -0,0 +1,27 @@
+﻿using ModelLayer.DTO;
+namespace RepositoryLayer.Service
+{
+    public class RegisterUserRL
+    {
+        private List<LoginDTO> users=new List<LoginDTO>();    
+        public RegisterUserRL() {}
+        public string RegisterRL()
+        {
+            return "This is Repository layer";
+        }
+
+        public bool RegisterUser(LoginDTO newUser)
+        {
+            if(users.Any(user => user.Email == newUser.Email))
+            {
+                return false;
+            }
+            users.Add(newUser);
+            return true;
+        }
+        public LoginDTO GetUserByEmail(string email)
+        { 
+            return users.FirstOrDefault(user => user.Email ==email);
+        }
+    }
+}
